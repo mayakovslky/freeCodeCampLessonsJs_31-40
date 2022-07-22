3296d986bb62137efe3a4fb2f73b14d6edccbb05
Vladimir Drozdov <mayakovsky03@gmail.com>
2022-07-22 15:42:36 +0300

    lessonsJs 31-40

---
+++ script.js
@@ -0,0 +1,60 @@
+// 31. Appending Variables to Strings
+
+// const someAdjective = "cool!";
+// let myStr = "Learning to code is ";
+// myStr += someAdjective;
+
+// 32. Find the Length of a String
+
+// let lastNameLength = 0;
+// const lastName = "Lovelace";
+
+// lastNameLength = lastName.length;
+
+// 33. Use Bracket Notation to Find the First Character in a String
+
+// let firstLetterOfLastName = "";
+// const lastName = "Lovelace";
+
+// firstLetterOfLastName = lastName[0];
+
+// 34. Understand String Immutability
+
+// let myStr = "Jello World";
+
+// myStr = "Hello World";
+
+// 35. Use Bracket Notation to Find the Nth Character in a String
+
+// const lastName = "Lovelace";
+
+// const thirdLetterOfLastName = lastName[2];
+
+// 36. Use Bracket Notation to Find the Last Character in a String
+
+// const lastName = "Lovelace";
+
+// const lastLetterOfLastName = lastName[lastName.length - 1]
+
+// 37. Use Bracket Notation to Find the Nth-to-Last Character in a String
+
+// const lastName = "Lovelace";
+
+// const secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+// 38. Word Blanks
+
+// const myNoun = "dog";
+// const myAdjective = "big";
+// const myVerb = "ran";
+// const myAdverb = "quickly";
+
+// const wordBlanks = "This " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+
+// 39. Store Multiple Values in one Variable using JavaScript Arrays
+
+const myArray = ["beat", 15];
+
+// 40. Nest one Array within Another Array
+
+// const myArray = [["big", 200], ["small", 20]];
